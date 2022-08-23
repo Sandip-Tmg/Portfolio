a49db69c8a1bfcb1c9345a60a1b1ad0ec00b4a53
Sandip Tamang <sandip.tamang@student.griffith.ie>
2022-08-23 12:09:35 +0100

    first commit

---
+++ src/components/Nav.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Nav() {
+  return (
+    <div className='fixed w-full h-[5rem] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'>
+        
+    </div>
+  )
+}
